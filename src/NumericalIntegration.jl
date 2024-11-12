9b81b25417c409a89b81991af544a02bb037460b
aconitum3 <aconitum@example.com>
2024-11-12 15:28:17 +0900

    Add Trapezoidal Rule. Modified Simpson Rule. ( improvement calculation cost )

--- src/NumericalIntegration.jl
+++ src/NumericalIntegration.jl
@@ -4,10 +4,23 @@ function SimpsonRule(f::Function,I::ClosedInterval{T₁};N::T₂=2^6,kwargs...) 
     if a == b
         return J
     end
-    x = a:(b-a)/(N-1):b
-    for n in 2:N
-        J += (x[n]-x[n-1])/6.0 * ( f(x[n-1]) + 4.0*f((x[n-1]+x[n])/2.0) + f(x[n]))
+    x = a:(b-a)/(2N):b
+    y = f.(x)
+    for n in 2:N+1
+        J += (x[2n-1]-x[2n-3])/6.0 * ( y[2n-3] + 4.0*y[2n-2] + y[2n-1] )
     end
+    return J
+end
+
+function TrapezoidalRule(f::Function,I::ClosedInterval{T₁};N::T₂=2^6,kwargs...) where {T₁<:Real, T₂<:Integer} 
+    x = a:(b-a)/(N-1):b
+    y = f.(x)
+    J = 0*f(a).^0
+    for n in 2:N
+        J += y[n-1] + y[n]
+    end
+
+    J = J * (b-a)/(2(N-1))
     return J
 end
 
@@ -29,4 +42,12 @@ function DESimpsonRule(f::Function,I::ClosedInterval{T};kwargs...) where T<:Real
         return 0*f(a).^0
     end
     return DoublyExponentialize(f,I;kwargs...) |> v -> SimpsonRule(v...;kwargs...)
+end
+
+function DETrapezoidalRule(f::Function,I::ClosedInterval{T};kwargs...) where T<:Real
+    a,b = I.left,I.right
+    if a == b
+        return 0*f(a).^0
+    end
+    return DoublyExponentialize(f,I;kwargs...) |> v -> TrapezoidalRule(v...;kwargs...)
 end
