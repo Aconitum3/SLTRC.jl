c94ba0492943f5668015d79ed36451970876e86d
aconitum3 <aconitum@example.com>
2024-11-26 12:00:08 +0900

    Fixed some error that raised when cL = 0 or cR = Inf

--- src/Likelihood/Exponential.jl
+++ src/Likelihood/Exponential.jl
@@ -3,8 +3,12 @@ function C(FX::Exponential{T₁},FY::Exponential{T₁},ObservationInterval::Clos
     cR = ObservationInterval.right
     θX, θY = params(FX)[1], params(FY)[1]
     
-    if cR == Inf
+    if cR == Inf && cL == 0
+        return 1.0
+    elseif cR == Inf && cL != 0 
         return exp(-cL/θX) + θY/(θY-θX) * (exp(-cL/θY) - exp(-cL/θX))
+    elseif cR != Inf && cL == 0
+        return 1.0 - exp(-cR/θX)
     else
         return exp(-cL/θX) - exp(-cR/θX) + θY/(θY-θX) * (exp(-cL/θY) - exp(-cL/θX))
     end
@@ -35,7 +39,11 @@ end
 
 function ∇yC(FX::Exponential{T₁},FY::Exponential{T₁},ObservationInterval::ClosedInterval{T₂}) where {T₁<:Real,T₂<:Real}
     θY = params(FY)[1]
-    return gradient(θ -> C(FX,Exponential(θ),ObservationInterval),θY)[1]
+    if ObservationInterval.left == 0
+        return 0.0
+    else
+        return gradient(θ -> C(FX,Exponential(θ),ObservationInterval),θY)[1]
+    end
 end
 
 function ∇²C(FX::Exponential{T₁},FY::Exponential{T₁},ObservationInterval::ClosedInterval{T₂}) where {T₁<:Real,T₂<:Real}
@@ -52,7 +60,11 @@ end
 
 function ∇²yC(FX::Exponential{T₁},FY::Exponential{T₁},ObservationInterval::ClosedInterval{T₂}) where {T₁<:Real,T₂<:Real}
     θY = params(FY)[1]
-    return hessian(θ -> C(FX,Exponential(θ),ObservationInterval),θY)
+    if ObservationInterval.left == 0
+        return 0.0
+    else
+        return hessian(θ -> C(FX,Exponential(θ),ObservationInterval),θY)
+    end
 end
 
 function logp̃(d::StrictlyLeftTruncatedData,FX::Exponential{T₁},FY::Exponential{T₂},ObservationInterval::ClosedInterval{T₂}) where {T₁<:Real,T₂<:Real}
@@ -79,6 +91,9 @@ function E∇²loglikelihood(FX::Exponential{T₁},FY::Exponential{T₁},Observa
     eRx, eRy = exp(-cR/μx), exp(-cR/μy)
     eRLy = exp(-(cR-cL)/μy)
 
+    cReRx = cR == Inf ? 0 : cR*eRx
+    cReRy = cR == Inf ? 0 : cR*eRy
+
     y_yx = μy/(μy-μx)
     x_yx = μx/(μy-μx)
 
@@ -88,30 +103,31 @@ function E∇²loglikelihood(FX::Exponential{T₁},FY::Exponential{T₁},Observa
     # P( Data is Complete )
     PC = (1 - y_yx * eRLy)*eLx + x_yx * eRx
     # E[Y1(Data is Complete)]
-    EY_C = μy*(eLx-eRx) - y_yx * (cR + μy - μx*y_yx) * (eLx*eRLy-eRx) + y_yx * (cL*eLx*eRLy-cR*eRx)
-    
+    EY_C = μy*(eLx-eRx)
+    if cR != Inf
+        EY_C += - y_yx * (cR + μy - μx*y_yx) * (eLx*eRLy-eRx) + y_yx * (cL*eLx*eRLy-cR*eRx)
+    end
     # P( Data is Left-Truncated )
     PLT = y_yx * (eLy-eLx-eRy+eLx*eRLy)
     # [Z1(Data is Left-Truncated)] 
-    EZ_LT = y_yx * (cL*eLy-cR*eRy + μy*(eLy-eRy)) * (1-eLx/eLy)
+    EZ_LT = y_yx * (cL*eLy-cReRy + μy*(eLy-eRy)) * (1-eLx/eLy)
     
     # P( Data is Right-Censored )
     PRC = y_yx * (eLx*eRLy-eRx)
     # E[X1(Data is Right-Censored)]
-    EX_RC = y_yx * (cL*eLx*eRLy - cR*eRx + μx*y_yx * (eLx*eRLy-eRx)) 
+    EX_RC = y_yx * (cL*eLx*eRLy - cReRx + μx*y_yx * (eLx*eRLy-eRx)) 
     
     # P( Data is Left-Truncated and Right-Censored )
     PLTRC = y_yx * eRy * (1-eLx/eLy)
-    
+   
     #===================================================#
-
-    ∇²A = hessian(v->log(1/(v-μx)*(1-exp(-cL*(1/μx-1/v)))),μy)
+    ∇²A = cL == 0 ? 0 : hessian(v->log(1/(v-μx)*(1-exp(-cL*(1/μx-1/v)))),μy)
     ∇²B = ∇ᵏylogC(FX,FY,ObservationInterval)[2]
     
     C_term  = -2/μy^3 * EY_C + (1/μy^2-∇²B)*PC
     LT_term = - 2/μy^3 * EZ_LT + (∇²A-∇²B)*PLT
-    RC_term = 2/μy^3 * EX_RC + (-2cR/μy^3-∇²B)*PRC
-    LTRC_term = (∇²A - 2cR/μy^3 - 1/μy^2 - ∇²B)*PLTRC
+    RC_term = cR == Inf ? 0 : 2/μy^3 * EX_RC + (-2cR/μy^3-∇²B)*PRC
+    LTRC_term = cR == Inf ? 0 : (∇²A - 2cR/μy^3 - 1/μy^2 - ∇²B)*PLTRC
     
     return (C_term+LT_term+RC_term+LTRC_term)/C_val
 end
